14791594c0ba2bf5d88787913d2b693d2ff7e7bf
KipodHater <ethandror123@gmail.com>
2025-01-27 22:42:24 +0200

    fixed robot file

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,17 +7,20 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.subsystems.SwerveSubsystem;
 
 public class Robot extends TimedRobot {
   public static final String CTREConfigs = null;
 
+public static Object ctreConfigs;
+private SwerveSubsystem swerve;
 private Command m_autonomousCommand;
 
   private final RobotContainer m_robotContainer;
 
   public Robot() {
     m_robotContainer = new RobotContainer();
-    // Logger.getins
+    swerve = new SwerveSubsystem();
     
   }
 
@@ -25,7 +28,7 @@ private Command m_autonomousCommand;
   public void robotPeriodic() {
     CommandScheduler.getInstance().run();
     m_robotContainer.periodic();
-    
+    swerve.periodic();
   }
 
   @Override
