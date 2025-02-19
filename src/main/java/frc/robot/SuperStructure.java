58fd71e99dafbaa82d6784e08e5e3d9d94a2ab47
roy2872 <roylevy1204@gmail.com>
2025-02-19 13:59:56 +0200

    fixed hopefully auto instance

--- src/main/java/frc/robot/SuperStructure.java
+++ src/main/java/frc/robot/SuperStructure.java
@@ -31,7 +31,7 @@ public class SuperStructure {
         // auto = new Autonomous();
         swerve = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(), "swerve"));
         
-        auto = new Autonomous();
+        auto = new Autonomous(this);
 
 
         // new WaitCommand(0.1).andThen(() -> arm.setDefaultCommand(
@@ -86,8 +86,8 @@ public class SuperStructure {
     }
 
     public Command getAutonomousCommand() {
-        // return auto.getSelected();
-        return null;
+        return auto.getSelected();
+        // return null;
     }
 
     public void enabledInit(){
