17764b11e855cdaba00fc9e43c14f23b2d65eeab
KipodHater <ethandror123@gmail.com>
2025-04-16 14:00:22 +0300

    not working

---
+++ src/main/java/frc/robot/subsystems/apriltagvision/AprilTagVision.java
@@ -0,0 +1,20 @@
+package frc.robot.subsystems.apriltagvision;
+
+import frc.lib.util.VirtualSubsystem;
+
+public class AprilTagVision extends VirtualSubsystem {
+    
+    private AprilTagVision(){
+
+    }
+
+    public AprilTagVision getInstance(){
+        if()
+    }
+
+
+    public void periodic(){
+
+    }
+
+}
